50bb604ce5a7e4a40982b3576a72b78a3dd218ee
matthewt-assurity <matthew.tansley@assurity.co.nz>
2021-07-16 11:54:46 +1200

    Introduced jsonPath and getJsonObject methods

--- src/test/java/steps/DemoSteps.java
+++ src/test/java/steps/DemoSteps.java
@@ -4,9 +4,11 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoSteps {
@@ -17,22 +19,22 @@ public class DemoSteps {
     public void iCheckTheDetailsOfStudent(int arg0) {
         RestAssured.baseURI = "https://it-foundations.app.ap.assurity.cloud/";
         RestAssured.defaultParser = Parser.JSON;
-        RestAssured.given().contentType("application/json\r\n");
+        given().contentType(ContentType.JSON);
         res = RestAssured.get("people/" + arg0);
         System.out.println(res.asPrettyString());
     }
 
     @Then("I can see that their name is {string}")
     public void iCanSeeThatTheirNameIsJaneJones(String arg0) {
-        String name = res.path("firstName") + " " + res.path("lastName");
+        String name = res.jsonPath().getJsonObject("firstName") + " " + res.jsonPath().getJsonObject("lastName");
         assertEquals(arg0, name);
         System.out.println(name);
     }
 
     @And("they have a {string} from {string}")
     public void theyHaveABScComputerScienceFromSydneyTechSchool(String arg0, String arg1) {
-        String degree = res.path("degree").toString();
-        String university = res.path("university").toString();
+        String degree = res.jsonPath().getJsonObject("degree").toString();
+        String university = res.jsonPath().getJsonObject("university").toString();
         assertEquals(arg0, degree);
         assertEquals(arg1, university);
         System.out.println(degree);
